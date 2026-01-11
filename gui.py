9faa0bc4d602066fbeabea2e4d5cce5d8eabfcdd
Daya3611 <hi@dayanandgawade.in>
2026-01-11 16:03:16 +0530

    update 2.0

--- gui.py
+++ gui.py
@@ -14,7 +14,7 @@ import matplotlib.pyplot as plt
 from word_detector import detect, prepare_img, sort_multiline
 from transformers import pipeline
 
-# Set page config
+# Set page confixg
 st.set_page_config(page_title="Handwritten Text Recognition", layout="wide")
 
 # Constants
